b56b673bd3b15823250798304ac453e8a5032aad
wh524778571 <524778571@qq.com>
2016-04-07 18:51:32 +0800

    201604072

--- app/src/main/java/com/myhome/wh/mybdmap/MainActivity.java
+++ app/src/main/java/com/myhome/wh/mybdmap/MainActivity.java
@@ -1,8 +1,8 @@
 package com.myhome.wh.mybdmap;
 
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,14 +14,18 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.LogoPosition;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.Marker;
-import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
@@ -31,6 +35,8 @@ import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
 import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.poi.PoiSearch;
 import com.myhome.wh.mybdmap.Overlay.PoiOverlay;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
     MapView mMapView = null;
@@ -43,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private CheckBox traffic;
     private CheckBox baiduHeatMap;
-    private Marker marker;
+
     private PoiSearch mPoiSearch;
     private Button check;
     private EditText etCity;
@@ -53,8 +59,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 定位SDK的核心类
      */
     public LocationClient mLocationClient = null;
-    public BDLocationListener myListener = new MyLocationListener();
+    public BDLocationListener myListener = new MyLocationListenner();
     private Button btlocation;
+
+    BitmapDescriptor mCurrentMarker;// Marker图标
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,26 +101,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //检索方法
         Poi();
+
+        // 构建mark图标
+        mCurrentMarker = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_marker);
         //  定位
         mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
         mLocationClient.registerLocationListener(myListener);    //注册监听函数
         initLocation();
 
 
-
-
-
-
-
     }
+
     //配置定位SDK参数
-    private void initLocation(){
+    private void initLocation() {
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         // 定位SDK可以返回bd09、bd09ll、gcj02三种类型坐标，若需要将定位点的位置通过百度Android地图 SDK进行地图展示，请返回bd09ll，将无偏差的叠加在百度地图上。
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span=1000;
+        int span = 1000;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true);//可选，默认false,设置是否使用gps
@@ -122,6 +131,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
         option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
         mLocationClient.setLocOption(option);
+    }
+
+
+    private void overlay(LatLng point, BitmapDescriptor bitmap,
+                         BaiduMap baiduMap) {
+        // 构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions().position(point)
+                .icon(bitmap);
+
+        // 在地图上添加Marker，并显示
+        baiduMap.addOverlay(option);
     }
 
 
@@ -145,6 +165,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     overlay.zoomToSpan();//缩放地图至合适视野
                 }
             }
+
             public void onGetPoiDetailResult(PoiDetailResult result) {
                 //获取Place详情页检索结果
                 if (result.error != SearchResult.ERRORNO.NO_ERROR) {
@@ -153,6 +174,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(MainActivity.this, result.getName() + ":" + result.getAddress(), Toast.LENGTH_LONG).show();
                 }
             }
+
             class MyPoiOverlay extends PoiOverlay {
                 /**
                  * 构造函数
@@ -163,6 +185,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     super(baiduMap);
 
                 }
+
                 @Override
                 public boolean onPoiClick(int i) {
                     PoiInfo poiInfo = getPoiResult().getAllPoi().get(i);
@@ -221,14 +244,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //执行搜索 获取两个输入框值
                 etCity = (EditText) findViewById(R.id.etCity);
                 etSearchkey = (EditText) findViewById(R.id.etSearchkey);
-                Toast.makeText(getApplicationContext(),"搜索"+etCity.getText()+":"+etSearchkey.getText(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "搜索" + etCity.getText() + ":" + etSearchkey.getText(), Toast.LENGTH_SHORT).show();
                 mPoiSearch.searchInCity((new PoiCitySearchOption())
                         .city(etCity.getText().toString())
                         .keyword(etSearchkey.getText().toString())
                         .pageNum(10));
                 break;
-            case  R.id.location:
-                Toast.makeText(getApplicationContext(),"我的位置",Toast.LENGTH_SHORT).show();
+            case R.id.location:
+                Toast.makeText(getApplicationContext(), "我的位置", Toast.LENGTH_SHORT).show();
                 mLocationClient.start();
                 break;
         }
@@ -260,6 +283,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mBaiduMap.setBaiduHeatMapEnabled(false);
                 }
                 break;
+        }
+    }
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListenner implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+
+
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null)
+                return;
+
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                            // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            // 设置缩放比例,更新地图状态
+            float f = mBaiduMap.getMaxZoomLevel();// 19.0 最小
+            MapStatusUpdate u = MapStatusUpdateFactory.newLatLngZoom(ll,
+                    f - 4);
+            mBaiduMap.animateMapStatus(u);
+            //  tv.setText(location.getAddrStr());
+            overlay(ll, mCurrentMarker, mBaiduMap);
+
+
         }
     }
 }
